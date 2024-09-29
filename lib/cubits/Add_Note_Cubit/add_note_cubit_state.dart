6e8ea9b262a5fc06c4b30bc6d0439970fe8b74ec
ahmedEhab10 <ahmedhobaa24@gmail.com>
2024-09-29 07:42:13 +0300

    create states

--- lib/cubits/Add_Note_Cubit/add_note_cubit_state.dart
+++ lib/cubits/Add_Note_Cubit/add_note_cubit_state.dart
@@ -4,3 +4,13 @@ part of 'add_note_cubit_cubit.dart';
 sealed class AddNoteCubitState {}
 
 final class AddNoteCubitInitial extends AddNoteCubitState {}
+
+final class AddNoteLoading extends AddNoteCubitState {}
+
+final class AddNoteCubitSccusses extends AddNoteCubitState {}
+
+final class AddNoteCubitFulier extends AddNoteCubitState {
+  final String ErrorMassage;
+
+  AddNoteCubitFulier({required this.ErrorMassage});
+}
