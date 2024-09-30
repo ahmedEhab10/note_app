041ff57e8651e97b5c8962d5ae2e8752b7d4d912
ahmedEhab10 <ahmedhobaa24@gmail.com>
2024-09-30 08:06:37 +0300

    add bloc provaider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/const.dart';
+import 'package:note_app/cubits/Add_Note_Cubit/add_note_cubit_cubit.dart';
 import 'package:note_app/models/NoteModel.dart';
 import 'package:note_app/views/Edit_Note_View.dart';
 import 'package:note_app/views/Notes_view.dart';
@@ -19,13 +21,20 @@ class NoteApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
-      routes: {
-        NotesView.id: (context) => NotesView(),
-        EditNoteView.id: (context) => EditNoteView()
-      },
-      initialRoute: NotesView.id,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AddNoteCubitCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+        routes: {
+          NotesView.id: (context) => NotesView(),
+          EditNoteView.id: (context) => EditNoteView()
+        },
+        initialRoute: NotesView.id,
+      ),
     );
   }
 }
